383cf9865d8a31a458d03eb7884bbadfdb7114f6
melramos5 <melramos479@gmail.com>
2024-03-05 21:22:33 -0300

    Initial commit

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('users')->insert([
+            'name' => 'Mel Ramos',
+            'email' => 'melramos352@gmail.com',
+            'password' => Hash::make('12345678'),
+        ]);
+
+        DB::table('users')->insert([
+            'name' => 'Mel Ramos',
+            'email' => 'melramos352@gmail.com',
+            'password' => Hash::make('12345678'),
+        ]);
+
+        DB::table('users')->insert([
+            'name' =>'Mel Ramos',
+            'email' => 'melramos352@gmail.com',
+            'password' => Hash::make('12345678'),
+        ]);
+    }
+}
